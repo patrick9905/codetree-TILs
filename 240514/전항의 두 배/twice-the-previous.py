02815297a5f7e2f4094a20e2407c027d182dceb1
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-05-14 03:16:03 +0900

    Title: 전항의 두 배, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240514/전항의 두 배/twice-the-previous.py
+++ 240514/전항의 두 배/twice-the-previous.py
@@ -1,8 +1,8 @@
 arr=[0]*10
 a,b=map(int,input().split())
-arr.append(a)
-arr.append(b)
-for i in range(3,10):
+arr[0]=a
+arr[1]=b
+for i in range(2,10):
     arr[i]=arr[i-1]+arr[i-2]*2
 
 for i in arr:
