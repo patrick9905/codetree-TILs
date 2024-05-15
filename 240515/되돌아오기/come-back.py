e2378d8ee785a2d1ac5808034926e9d3560d3806
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-05-15 14:50:42 +0900

    Title: 되돌아오기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240515/되돌아오기/come-back.py
+++ 240515/되돌아오기/come-back.py
@@ -1,26 +1,33 @@
-n=input()
-x,y=0,0
-ans=0
-dx,dy=[0,1,0,-1],[1,0,-1,0]
-reach_home=False
-for i in range(int(n)):
-    direction,distance=input().split()
-    if direction=='N':
-        dir_num=0
-    elif direction=='E':
-        dir_num=1
-    elif direction=='S':
-        dir_num=2
-    elif direction=='W':
-        dir_num=3
-    for j in range(int(distance)):
-        x+=dx[dir_num]
-        y+=dy[dir_num]
-        ans+=1
-        if x==0 and y==0 :
+n = int(input())
+x, y = 0, 0
+ans = 0
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+reach_home = False
+
+for i in range(n):
+    direction, distance = input().split()
+    distance = int(distance)
+    
+    if direction == 'N':
+        dir_num = 0
+    elif direction == 'E':
+        dir_num = 1
+    elif direction == 'S':
+        dir_num = 2
+    elif direction == 'W':
+        dir_num = 3
+
+    for j in range(distance):
+        x += dx[dir_num]
+        y += dy[dir_num]
+        ans += 1
+        if x == 0 and y == 0:
             print(ans)
-            reach_home=True
+            reach_home = True
             break
+    
+    if reach_home:
+        break
 
 if not reach_home:
     print(-1)
