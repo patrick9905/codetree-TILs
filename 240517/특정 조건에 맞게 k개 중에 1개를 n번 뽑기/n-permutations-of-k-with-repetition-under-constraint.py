e85e868ae1d105d7d5f2284284cafab810c9d441
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-05-17 20:30:26 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 357ms, Memory: 29MB, Status: Passed - CodeTree

--- 240517/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240517/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -1,22 +1,17 @@
 k,n=tuple(map(int,input().split()))
+
 a=[]
-
-
-def p():
-    for i in a:
-        print(i,end=' ')
-    print()
 def choose(num):
-    if num==n:
-        for i in range(n):
-            if a[i-1]==a[i-2] and a[i-2]==a[i-3]:
-                return
-        p()
+    if num>=n:
+        print(*a)
         return
-    for i in range(1,k+1):
         
+    for i in range(1,k+1):
+        if num>=2 and a[-1]==a[-2] and a[-1]==i:
+            continue
         a.append(i)
         choose(num+1)
         a.pop()
+
 
 choose(0)
