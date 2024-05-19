4712b1564f53fa331e0580da484729faf7550065
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-05-19 15:19:35 +0900

    Title: 점수대 파악하기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240519/점수대 파악하기/find-out-the-score-range.py
+++ 240519/점수대 파악하기/find-out-the-score-range.py
@@ -1,7 +1,9 @@
 list_num=list(map(int,input().split()))
 list_score=[0]*11
 for i in list_num:
+    if i==0:
+        break
     list_score[i//10]+=1
-list_score[0]=0
+    
 for i in range(10,0,-1):
     print(f"{i}0 - {list_score[i]}")
