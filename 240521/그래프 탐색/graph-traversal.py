8d163407468e2a83ee33b363b9d51605b451b9d7
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-05-21 19:58:10 +0900

    Title: 그래프 탐색, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240521/그래프 탐색/graph-traversal.py
+++ 240521/그래프 탐색/graph-traversal.py
@@ -5,14 +5,13 @@ for _ in range(m):
     a,b=map(int,input().split())
     arr[a][b]=1
     arr[b][a]=1
-cnt=0
+
 def dfs(v):
-    global cnt
     for i in range(1,m+1):
         if arr[v][i] and not visited[i]:
             visited[i]=True
-            cnt+=1
+
             dfs(i)
 visited[1]=1
 dfs(1)
-print(cnt)
+print(sum(visited))
