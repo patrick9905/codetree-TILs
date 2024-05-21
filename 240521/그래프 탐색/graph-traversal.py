4a19ae0d34d075d5a274b8dfcae42e7cf43c45cd
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-05-21 20:26:06 +0900

    Title: 그래프 탐색, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240521/그래프 탐색/graph-traversal.py
+++ 240521/그래프 탐색/graph-traversal.py
@@ -1,17 +1,18 @@
-n,m=map(int,input().split())
+n, m = map(int, input().split())
 arr = [[0] * (n + 1) for _ in range(n + 1)]
-visited=[0 for _ in range(n+1)]
+visited = [0 for _ in range(n + 1)]
+
 for _ in range(m):
-    a,b=map(int,input().split())
-    arr[a][b]=1
-    arr[b][a]=1
+    a, b = map(int, input().split())
+    arr[a][b] = 1
+    arr[b][a] = 1  # 무방향 그래프이므로 양쪽 다 연결
 
 def dfs(v):
-    for i in range(1,m+1):
+    for i in range(1, n + 1):  # n을 사용하여 노드 범위를 순회
         if arr[v][i] and not visited[i]:
-            visited[i]=True
-
+            visited[i] = 1  # 방문 표시를 1로
             dfs(i)
-visited[1]=1
+
+visited[1] = 1  # 시작 노드를 방문 표시
 dfs(1)
-print(sum(visited))
+print(sum(visited))  # 방문한 노드의 개수를 출력
