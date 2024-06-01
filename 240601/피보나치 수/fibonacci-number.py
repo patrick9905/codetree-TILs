ae358b623f28999f9f519ecf37bf31dd13b2e41c
patrick9905 <104254505+patrick9905@users.noreply.github.com>
2024-06-01 22:41:36 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240601/피보나치 수/fibonacci-number.py
@@ -0,0 +1,12 @@
+def pibo(n):
+    if memo[n]!=-1:
+        return memo[n]:
+    if n<=2:
+        memo[n]=1
+    else:
+        memo[n]=pibo[n-1]+pibo[n-2]
+    return memo[n]
+
+
+n=int(input())
+print(pibo(n))
